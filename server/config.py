b650086cea498766f28d4d0b09cd87dda55c726f
Pedro Alarcon <pedroa54@icloud.com>
2024-06-04 12:15:46 +0200

    adds first 3 models

--- server/config.py
+++ server/config.py
@@ -1,19 +1,27 @@
-# Standard library imports
-
 # Remote library imports
+from os import environ
+from dotenv import load_dotenv
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-# Local imports
+# Load environment variables from .env file
+load_dotenv(".env")
 
 # Instantiate app, set attributes
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pet_hotel.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Set the secret key for session management
+app.secret_key = environ.get("SESSION_SECRET")
+
+bcrypt = Bcrypt(app)
+api = Api(app)
 app.json.compact = False
 
 # Define metadata, instantiate db
@@ -26,8 +34,13 @@ db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
 db.init_app(app)
 
-# Instantiate REST API
-api = Api(app)
-
 # Instantiate CORS
 CORS(app)
+
+# Define your API resources and routes here
+
+if __name__ == "__main__":
+    app.run(
+        port=environ.get("FLASK_RUN_PORT", 5555),
+        debug=environ.get("FLASK_DEBUG", False),
+    )
--- server/models.py
+++ server/models.py
@@ -1,6 +1,75 @@
-from sqlalchemy_serializer import SerializerMixin
+from config import bcrypt, db
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Date, ForeignKey, Integer
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+import itertools
 
-from config import db
+# Models
 
-# Models go here!
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(100), nullable=False)
+    _password_hash = db.Column(db.String(128), nullable=False)
+
+    # Relationships
+
+    @validates("userName")
+    def validate_userName(self, _, userName):
+        if not userName:
+            raise ValueError("userName cannot be empty")
+        if len(userName) > 100:
+            raise ValueError("userName cannot exceed 100 characters")
+        return userName
+
+    @hybrid_property
+    def bookings(self):
+        return list(itertools.chain(*self.bookings_per_animal))
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("password is not readable")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password)
+
+    def __repr__(self):
+        return f"User(id={self.id}, userName='{self.userName}')"
+
+
+class Recipe:
+    __tablename__ = "recipes"
+    id = db.Column()
+    title = db.Column()
+    description = db.Column()
+    ingredients = db.Column()
+    instructions = db.Column()
+
+    # Relationships
+
+    def __repr__(self):
+        return f"Recipe(id={self.id}, title='{self.title}', description='{self.description}', ingredients='{self.ingredients}', instructions='{self.instructions}')"
+
+
+class Tag:
+    __tablename__ = "tags"
+    id = db.Column()
+    category = db.Column()
+
+    # Relationships
+
+
+class RecipeTag:
+    __tablename__ = ""
+
+
+class Like:
+    __tablename__ = ""
