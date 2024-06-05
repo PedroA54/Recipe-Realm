4582ba15a1d60e8686549ee716850fd7319e15a9
Pedro Alarcon <pedroa54@icloud.com>
2024-06-05 12:03:22 +0200

    removes likes and adds comments model

--- server/models.py
+++ server/models.py
@@ -1,15 +1,14 @@
 from config import bcrypt, db
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Date, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
 import itertools
 
-# Models
 
-
+# User #
 class User(db.Model, SerializerMixin):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
@@ -21,10 +20,6 @@ class User(db.Model, SerializerMixin):
     liked_recipes = db.relationship(
         "Recipe", secondary="likes", backref=db.backref("likes", lazy="dynamic")
     )
-
-    # Serialization
-    # Excludes recipes, liked recipes, and password ofc
-    serialize_rules = ("-recipes.user", "-liked_recipes.liked_by", "-_password_hash")
 
     @validates("userName")
     def validate_userName(self, _, userName):
@@ -49,6 +44,7 @@ class User(db.Model, SerializerMixin):
         return f"User(id={self.id}, userName='{self.userName}')"
 
 
+# Recipe #
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = "recipes"
     id = db.Column(db.Integer, primary_key=True)
@@ -56,50 +52,57 @@ class Recipe(db.Model, SerializerMixin):
     description = db.Column(db.Text, nullable=False)
     ingredients = db.Column(db.Text, nullable=False)
     instructions = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"), nullable=False)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    tag_id = db.Column(
+        db.Integer, db.ForeignKey("tags.id", ondelete="CASCADE"), nullable=False
+    )
 
     # Relationships
     tags = db.relationship(
         "Tag", secondary="recipe_tags", backref=db.backref("recipes", lazy="dynamic")
     )
 
-    # Serialization
-    # Exclude user and tag preventing circular rrefernce
-    serialize_rules = ("-user.recipes", "-tags.recipes")
-
     def __repr__(self):
         return f"Recipe(id={self.id}, title='{self.title}', description='{self.description}', ingredients='{self.ingredients}', instructions='{self.instructions}')"
 
 
+# Food Type Tag #
 class Tag(db.Model, SerializerMixin):
     __tablename__ = "tags"
     id = db.Column(db.Integer, primary_key=True)
     category = db.Column(db.String(100), unique=True, nullable=False)
 
-    # Serialization
-    # Exclude recipes
-    serialize_rules = ("-recipes.tags",)
-
     def __repr__(self):
         return f"Tag(id={self.id}, category='{self.category}')"
 
 
+# Combines Tag With Recipe #
 class RecipeTag(db.Model, SerializerMixin):
     __tablename__ = "recipe_tags"
-    recipe_id = db.Column(db.Integer, db.ForeignKey("recipes.id"), primary_key=True)
-    tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"), primary_key=True)
+    recipe_id = db.Column(
+        db.Integer, db.ForeignKey("recipes.id", ondelete="CASCADE"), primary_key=True
+    )
+    tag_id = db.Column(
+        db.Integer, db.ForeignKey("tags.id", ondelete="CASCADE"), primary_key=True
+    )
 
 
 def __repr__(self):
     return f"RecipeTag(recipe_id={self.recipe_id}, tag_id={self.tag_id})"
 
 
-class Like(db.Model, SerializerMixin):
-    __tablename__ = "likes"
-    recipe_id = db.Column(db.Integer, db.ForeignKey("recipes.id"), primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
+class Comment(db.Model, SerializerMixin):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(
+        db.Integer, db.ForeignKey("recipes.id", ondelete="CASCADE"), primary_key=True
+    )
+    user_id = db.Column(
+        db.Integer, db.ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
 
 
 def __repr__(self):
-    return f"Like(recipe_id={self.recipe_id}, user_id={self.user_id})"
+    return f"<Comment id={self.id} recipe_id={self.recipe_id} user_id={self.user_id}>"
