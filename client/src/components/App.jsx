6510770d5064f560468d707cda4aaa2ae8c85c4c
Pedro Alarcon <pedroa54@icloud.com>
2024-06-10 21:40:50 +0200

    adds styling

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -8,6 +8,7 @@ import DetailPage from '../pages/DetailPage';
 import ManagementPage from '../pages/ManagementPage';
 import ProfilePage from '../pages/ProfilePage';
 import NavBar from './NavBar';
+import './NavBar.css';
 import '../style.css';
 
 function App() {
